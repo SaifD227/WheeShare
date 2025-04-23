70c2239f95a9f37b89a1af6ec169778520a2b182
zainwaseem <zainwaseem9371@gmail.com>
2025-04-23 12:48:17 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import { ThreeDCardDemo } from './components/ThreeDCardDemo'
+
+
+function App() {
+ 
+
+  return (
+    <div>
+      <h1 className='bg-red-900'>hadfsjfksf</h1>
+<ThreeDCardDemo/>
+    </div>
+  )
+}
+
+export default App
